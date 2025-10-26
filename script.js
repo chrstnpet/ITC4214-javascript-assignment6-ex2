65b664c99196c42cf6b3347da820a0c054af231b
christina <chrstnpet@gmail.com>
2025-10-26 22:41:31 +0200

    Created function to make Hello World text appear in the color of the color box

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-document.querySelector("#red").addEventListener('click', () => {
-    alert('Image clicked');
-})
+//document.querySelector("#red").addEventListener('click', () => {
+//    alert('Image clicked');
+//})
+
+function display(input) {
+    var color = input.id;
+    var final = document.querySelector(".printing-results");
+    final.innerHTML = "Hello World!";
+    final.style.color = color;
+}
